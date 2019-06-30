65a912e9f9128b9429da7e66216c6a88d3fbaba7
Kirill Diduk <kirill.diduk@mail.ru>
2019-06-30 14:14:27 +0200

    Use VisibilityNotifier

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -1,16 +1,11 @@
 extends Area2D
 
-export var speed = 50
-var screen_size
-var sprite_size
-
-func _ready():
-    screen_size = get_viewport_rect().size
-    sprite_size = $Sprite.get_rect().size
+export var speed: int = 50
 
 func _process(delta):
     move_local_y(-delta * speed)
-    if get_global_transform_with_canvas().origin.y < -sprite_size.y:
-        queue_free()
+
+func _on_screen_exited():
+    queue_free()
 
 # EOF
